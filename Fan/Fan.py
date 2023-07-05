5db9f18be2fdd01d0ecfc6b1b9e1607fe7259647
pochita0109 <akosicosta@gmail.com>
2023-07-04 18:58:42 -0700

    Pseudocodes

---
+++ Fan/Fan.py
@@ -0,0 +1,15 @@
+# Kenneth John Costa
+# Activity Fan
+
+# Create a Fan class
+# Create constants for slow, medium, and fast
+# Create a constructor for Fan class
+# Create a private instance variable 
+# Create an accessor (Getter) for speed
+# Create a mutator (Setter) for speed
+# Create an accessor (Getter) for switch
+# Create a mutator (Setter) for switch
+# Create an accessor (Getter) for radius
+# Create a mutator (Setter) for radius
+# Create an accessor (Getter) for color
+# Create a mutator (Setter) for color
