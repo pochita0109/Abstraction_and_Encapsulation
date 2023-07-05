3ba8ac1c5b14a205e077d0e0e320900121e9ac0e
pochita0109 <akosicosta@gmail.com>
2023-07-04 19:25:30 -0700

    Created fan class, set the constant, and created a constructor and private instance variable

--- Fan/Fan.py
+++ Fan/Fan.py
@@ -2,9 +2,22 @@
 # Activity Fan
 
 # Create a Fan class
+class Fan:
+
 # Create constants for slow, medium, and fast
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
+
 # Create a constructor for Fan class
+    def __init__(self, speed = SLOW, on = False, radius = 5, color = "blue"):
+     
 # Create a private instance variable 
+     self.__speed = speed
+     self.__on = on
+     self.__radius = radius
+     self.__color = color
+
 # Create an accessor (Getter) for speed
 # Create a mutator (Setter) for speed
 # Create an accessor (Getter) for switch
