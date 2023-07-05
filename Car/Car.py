b2b07fd11032648af7bbe10e898053c156d22914
pochita0109 <akosicosta@gmail.com>
2023-07-05 10:57:08 -0700

    Added pseudocodes on Car Activity

---
+++ Car/Car.py
@@ -0,0 +1,14 @@
+# Kenneth John Costa
+# Activity Car
+
+# Create a Car class
+# Create a constructor for Car class
+# Create a private instance variable    
+# Get the year of Car
+# Set the year of Car
+# Get the make of Car
+# Set the make of Car
+# Set the speed of Car
+# Create an accelerate method
+# Create a brake method
+# Create a get speed method
