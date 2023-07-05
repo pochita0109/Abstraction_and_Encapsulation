cb05866fe8e1e0f1fbdb12a89b2d8dc7731f20cc
pochita0109 <akosicosta@gmail.com>
2023-07-05 11:45:43 -0700

    Imported the car class, created a new class, and created a new object

--- Car/TestCar.py
+++ Car/TestCar.py
@@ -1,6 +1,12 @@
 # Import Car class from Car file
+from Car import Car
+
 # Create a class named TestCar
+class TestCar:
+    def Test_car(self):
 # Create a car object
-# Accelerate
+     car = Car(2023, "Honda", 0)
+     
+# Accelerate   
 # Brake
 # Print the result
