fe5b6708dca28b283b662a83ee56c9e38b0613bf
pochita0109 <akosicosta@gmail.com>
2023-07-05 07:57:35 -0700

    Imported the class, created two objects, and printed the results

--- Fan/TestFan.py
+++ Fan/TestFan.py
@@ -1,5 +1,24 @@
 # Import Fan class from Fan file
+from Fan import Fan
+
+def Test_Fan():
+
 # Fan1
+     Fan1 = Fan(Fan.FAST, True, 10, "Yellow")
+
 # Print the result of Fan1
+     print(Fan1.get_speed())
+     print(Fan1.get_switch())
+     print(Fan1.get_radius())
+     print(Fan1.get_color())
+
 # Fan2
+     Fan2 = Fan(Fan.MEDIUM, False, 5, "Blue")
+
 # Print the result of Fan2
+     print(Fan2.get_speed())
+     print(Fan2.get_switch())
+     print(Fan2.get_radius())
+     print(Fan2.get_color())
+
+Test_Fan()
