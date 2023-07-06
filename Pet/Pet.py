372702f6298a4a3e09c794be42b9e6fa36e0a9e6
pochita0109 <akosicosta@gmail.com>
2023-07-05 19:51:21 -0700

    Created a getter and setter methods for the name of pet

--- Pet/Pet.py
+++ Pet/Pet.py
@@ -8,12 +8,17 @@ class Pet:
     def __init__ (self, name, type, age):
 
 # Create a private instance variable
-        self.__name = name
-        self.__type = type
-        self.__age = age
-        
+     self.__name = name
+     self.__type = type
+     self.__age = age
+
 # Get the name of the pet
+    def get_name(self):
+        return self.__name
+        
 # Set the name of the pet
+    def set_name(self, name):
+        self.__name = name
 # Get the animal type of the pet
 # Set the animal type of the pet
 # Get the age of the pet
