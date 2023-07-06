914e116747bbd4c8683f7469ab0bc6ed36c2d05e
pochita0109 <akosicosta@gmail.com>
2023-07-05 21:54:57 -0700

    Created a new file and added new pesudocodes

---
+++ Pet/TestPet.py
@@ -0,0 +1,7 @@
+# Import Pet class from Pet file
+# Create a class name TestPet
+# Ask the user to enter the name of pet
+# Ask the user to enter the type of pet
+# Ask the user to enter the age of pet
+# Create pet object
+# Print the result
