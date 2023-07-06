7054724a5d4fdeea56130397a6e7400395cc793c
pochita0109 <akosicosta@gmail.com>
2023-07-05 22:01:09 -0700

    Asked the user to input the name, type, and age of their pet

--- Pet/TestPet.py
+++ Pet/TestPet.py
@@ -4,8 +4,15 @@ from Pet import Pet
 # Create a class name TestPet
 class TestPet:
     def test_pet(self):
+
 # Ask the user to enter the name of pet
+        name = input("Please enter the name of your pet: ")
+
 # Ask the user to enter the type of pet
+        type = input("Please enter what kind of animal is your pet: ")
+
 # Ask the user to enter the age of pet
+        age = input("Please enter the age of your pet in years: ")
+        
 # Create pet object
 # Print the result
