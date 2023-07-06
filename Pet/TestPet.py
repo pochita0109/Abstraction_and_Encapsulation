139d0c5d9795acd749f0935b4bf0f7704d5e38c4
pochita0109 <akosicosta@gmail.com>
2023-07-05 22:17:54 -0700

    Added designs

--- Pet/TestPet.py
+++ Pet/TestPet.py
@@ -6,21 +6,32 @@ class TestPet:
     def test_pet():
 
 # Ask the user to enter the name of pet
-        name = input("Please enter the name of your pet: ")
+        name = input("\033[92mPlease enter the name of your pet: ")
 
 # Ask the user to enter the type of pet
-        type = input("Please enter what kind of animal is your pet: ")
+        type = input("\033[92mPlease enter what kind of animal is your pet: ")
 
 # Ask the user to enter the age of pet
-        age = input("Please enter the age of your pet in years: ")
+        age = input("\033[92mPlease enter the age of your pet in years: ")
 
 # Create pet object
         pet = Pet(name, type, age)
 
 # Print the result
-        print(pet.get_name())
-        print(pet.get_type())
-        print(pet.get_age())
+        print()
+        print("\033[91m=" * 50)
+        print("\033[95m\033[1mYOUR PET INFORMATION".center(58))
+        print("\033[91m=" * 50)
+        print()
 
+        print("\033[93mPet Name:", pet.get_name())
+        print("\033[93mType:", pet.get_type())
+        print("\033[93mAge:", pet.get_age())
+
+        print()
+        print("\033[91m=" * 50)
+        print("\033[91m=" * 50)
+        print()
+        
 run = TestPet
 run.test_pet()
