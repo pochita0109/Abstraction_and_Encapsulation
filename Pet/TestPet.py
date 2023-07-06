0c907dabe477ff128fc60b411dd615e74817aabe
pochita0109 <akosicosta@gmail.com>
2023-07-05 21:57:25 -0700

    Imported the pet class from pet file and created new class named TestPet

--- Pet/TestPet.py
+++ Pet/TestPet.py
@@ -1,5 +1,9 @@
 # Import Pet class from Pet file
+from Pet import Pet
+
 # Create a class name TestPet
+class TestPet:
+    def test_pet(self):
 # Ask the user to enter the name of pet
 # Ask the user to enter the type of pet
 # Ask the user to enter the age of pet
