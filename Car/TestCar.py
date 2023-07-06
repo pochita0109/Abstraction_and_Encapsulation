dff5202217d4ff476fc523115a3cb5a83263f449
pochita0109 <akosicosta@gmail.com>
2023-07-05 18:03:53 -0700

    Did final changes, added designs

--- Car/TestCar.py
+++ Car/TestCar.py
@@ -8,47 +8,57 @@ class TestCar:
      car = Car(2023, "Honda", 0)
 
 # Print the result
+     print("\033[92mYEAR:", car.get_year())
+     print("\033[92mMODEL:", car.get_make())
+     print("\033[92mSPEED:", car.get_speed())
+
+     print()
+
 # Accelerate
+     print("\033[93mAccelerating....")
     # First call
      car.accelerate()
-     print(car.get_speed())
+     print("\033[93mCurrent Speed:", car.get_speed())
     
     # Second call
      car.accelerate()
-     print(car.get_speed())
+     print("\033[93mCurrent Speed:", car.get_speed())
 
     # Third call
      car.accelerate()
-     print(car.get_speed())
+     print("\033[93mCurrent Speed:", car.get_speed())
 
     # Fourth call
      car.accelerate()
-     print(car.get_speed())
+     print("\033[93mCurrent Speed:", car.get_speed())
 
     # Fifth call
      car.accelerate()
-     print(car.get_speed())   
+     print("\033[93mCurrent Speed:", car.get_speed())   
+     
+     print()
 
 # Brake
+     print("\033[91mDeccelerating....")
     # First call
      car.brake()
-     print(car.get_speed())
+     print("\033[91mCurrent Speed:", car.get_speed())
 
      # Second call
      car.brake()
-     print(car.get_speed())
+     print("\033[91mCurrent Speed:", car.get_speed())
 
      # Third call
      car.brake()
-     print(car.get_speed())
+     print("\033[91mCurrent Speed:", car.get_speed())
 
      # Fourth call
      car.brake()
-     print(car.get_speed())
+     print("\033[91mCurrent Speed:", car.get_speed())
 
      # Fifth call
      car.brake()
-     print(car.get_speed())
+     print("\033[91mCurrent Speed:", car.get_speed())
 
 run = TestCar()
 run.Test_car()
