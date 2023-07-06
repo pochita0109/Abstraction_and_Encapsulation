78cf8282b1c89a75002b8b04956c06c6de80f0c6
pochita0109 <akosicosta@gmail.com>
2023-07-05 19:37:51 -0700

    Created pet class, added constructors, and created a private instance variables

--- Pet/Pet.py
+++ Pet/Pet.py
@@ -2,8 +2,16 @@
 # Activity Pet
 
 # Create a Pet class
+class Pet:
+
 # Create a constructor for Pet class
+    def __init__ (self, name, type, age):
+
 # Create a private instance variable
+        self.__name = name
+        self.__type = type
+        self.__age = age
+        
 # Get the name of the pet
 # Set the name of the pet
 # Get the animal type of the pet
