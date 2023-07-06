08397b3a586f9526b2a9f2c92bf770cdeed0fe24
pochita0109 <akosicosta@gmail.com>
2023-07-05 22:07:43 -0700

    created a pet object and printed the results

--- Pet/TestPet.py
+++ Pet/TestPet.py
@@ -3,7 +3,7 @@ from Pet import Pet
 
 # Create a class name TestPet
 class TestPet:
-    def test_pet(self):
+    def test_pet():
 
 # Ask the user to enter the name of pet
         name = input("Please enter the name of your pet: ")
@@ -13,6 +13,14 @@ class TestPet:
 
 # Ask the user to enter the age of pet
         age = input("Please enter the age of your pet in years: ")
-        
+
 # Create pet object
+        pet = Pet(name, type, age)
+
 # Print the result
+        print(pet.get_name())
+        print(pet.get_type())
+        print(pet.get_age())
+
+run = TestPet
+run.test_pet()
