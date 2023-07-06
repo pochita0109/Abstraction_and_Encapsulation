d305b630f41a66f52dad027942cf1c91b4af436e
pochita0109 <akosicosta@gmail.com>
2023-07-05 17:43:38 -0700

    Called the accelerate method five times

--- Car/TestCar.py
+++ Car/TestCar.py
@@ -6,7 +6,30 @@ class TestCar:
     def Test_car(self):
 # Create a car object
      car = Car(2023, "Honda", 0)
-     
-# Accelerate   
+
+# Accelerate
+   # First call
+     car.accelerate()
+     print(car.get_speed())
+    
+    # Second call
+     car.accelerate()
+     print(car.get_speed())
+
+    # Third call
+     car.accelerate()
+     print(car.get_speed())
+
+    # Fourth call
+     car.accelerate()
+     print(car.get_speed())
+
+    # Fifth call
+     car.accelerate()
+     print(car.get_speed())   
+
 # Brake
 # Print the result
+
+run = TestCar()
+run.Test_car()
