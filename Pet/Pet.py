47ca21ec576dc9a2471cea19dd11cff8e27730cb
pochita0109 <akosicosta@gmail.com>
2023-07-05 19:32:54 -0700

    Added a pseudocodes for Pet class

---
+++ Pet/Pet.py
@@ -0,0 +1,12 @@
+# Kenneth John Costa
+# Activity Pet
+
+# Create a Pet class
+# Create a constructor for Pet class
+# Create a private instance variable
+# Get the name of the pet
+# Set the name of the pet
+# Get the animal type of the pet
+# Set the animal type of the pet
+# Get the age of the pet
+# Set the age of the pet
